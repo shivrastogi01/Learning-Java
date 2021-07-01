e1f741f2e911985d2ec8f57bb61d365f23502bd8
shivrastogi <shivrastogi01@gmail.com>
2021-07-01 08:43:57 +0530

    Programs on maximum width ,spiral form,balanced Binary tree

---
+++ WidthOfBinaryTree.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node  
+{ 
+  int key; 
+  Node left; 
+  Node right; 
+  Node(int k){
+      key=k;
+      left=right=null;
+  }
+}
+
+
+class WidthOfBinaryTree { 
+    
+    public static void main(String args[]) 
+    { 
+        Node root=new Node(10);
+    	root.left=new Node(20);
+    	root.right=new Node(30);
+    	root.left.left=new Node(40);
+    	root.left.right=new Node(50);
+    	root.right.left=new Node(60);
+    	
+        System.out.print(maxWidth(root));
+    } 
+    
+    public static int maxWidth(Node root){
+        if(root==null)return 0;
+        LinkedList<Node> q=new LinkedList();
+        q.add(root);
+        int res=0;
+        while(q.isEmpty()==false){
+            int count=q.size();
+            res=Math.max(res,count);
+            for(int i=0;i<count;i++){
+                Node curr=q.poll();
+                if(curr.left!=null)
+                    q.add(curr.left);
+                if(curr.right!=null)
+                    q.add(curr.right);
+            }
+        }
+        return res;
+    }   
+} 
