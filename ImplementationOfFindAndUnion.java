0f3e8963dbd0a3eecda9f2d2fb4945131dcd6dbf
shivrastogi <shivrastogi01@gmail.com>
2021-07-24 23:33:55 +0530

    union of set

---
+++ ImplementationOfFindAndUnion.java
@@ -0,0 +1,54 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import static java.lang.System.out;
+
+class ImplementationOfFindAndUnion {
+
+	static int parent[];
+
+
+	static void initialize()
+	{
+	    for(int i=0; i<parent.length; i++)
+	    {
+	        parent[i]=i;
+	    }
+	}
+
+	static int find(int x)
+	{
+	    if(parent[x]==x)
+	        return x;
+	    else
+	        return find(parent[x]);
+	    
+	}
+
+	static void union(int x, int y)
+	{
+	    int x_rep = find(x);
+	    int y_rep = find(y);
+	    
+	    if(x_rep==y_rep)
+	    return;
+	    
+	    parent[y_rep]=x_rep;
+	}
+
+	public static void main (String[] args) {
+		
+		int n = 5;
+
+		parent = new int[5];
+
+		initialize();
+
+		union(0, 2);
+		union(0, 4);
+
+		out.println(find(4));
+		out.println(find(3));
+	}
+}
