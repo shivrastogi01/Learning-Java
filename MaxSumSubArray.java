cf37022131a3f5534e6361364c21274c671a2c86
shivrastogi <shivrastogi01@gmail.com>
2021-10-02 19:46:35 +0530

    Max

---
+++ MaxSumSubArray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class MaxSumSubArray{
+	public static int subArraySum(int [] arr){
+		int maxSum=arr[0];
+		int currSum=maxSum;
+		for(int i=1;i<arr.length;i++){
+			currSum=Math.max(currSum + arr[i], arr[i]);
+			maxSum=Math.max(currSum,maxSum);
+		}
+		return maxSum;
+	}
+	public static void main(String [] args){
+		int arr[]={2};
+		System.out.println(subArraySum(arr));
+	}
+}
