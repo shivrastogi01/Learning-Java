c649186853c38c704f3700fb706bb2e7041e2b2a
shivrastogi <shivrastogi01@gmail.com>
2021-10-22 08:45:47 +0530

    maximum product subarray

---
+++ MaximumProductSubArray.java
@@ -0,0 +1,26 @@
+class MaximumProductSubArray {
+    public int maxProduct(int[] arr) {
+        int ans=Integer.MIN_VALUE;
+        int cprod=1;
+        for(int i=0;i<arr.length;i++)
+        {
+            cprod = cprod*arr[i];
+            ans = Math.max(ans,cprod);
+            if(cprod==0)
+            {
+                cprod=1;
+            }
+        }
+        cprod=1;
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            cprod = cprod*arr[i];
+            ans = Math.max(ans,cprod);
+            if(cprod==0)
+            {
+                cprod=1;
+            }
+        }
+        return ans;
+    }
+}
