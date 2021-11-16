b125ca2b5d2484e40f135d48450078e0ba9bbf62
shivrastogi <shivrastogi01@gmail.com>
2021-11-16 20:57:10 +0530

    Largest Triangle area

---
+++ LargestTriangleArea.java
@@ -0,0 +1,28 @@
+class LargestTriangleArea {
+
+public double largestTriangleArea(int[][] points) {
+    int n = points.length;
+    double max =0;
+    
+    for(int i =0; i<n; i++)
+        for(int j =i+1; j<n; j++)
+            for(int k =j+1; k<n; k++)
+            {
+                double area =0;
+                int[] a=points[i];
+                int[] b=points[j];
+                int[] c=points[k];
+                area=Math.abs(area(a,b)+area(b,c)+area(c,a));
+                if(area>max)
+                    max=area;
+            }
+    return max;
+}
+
+public double area(int[] a, int[] b)
+{
+    int l = b[0]-a[0];
+    double h = (a[1]+b[1]+200)/2.0;
+    return l*h;
+}
+}
