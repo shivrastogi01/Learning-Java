f7e02849fea98144b2814520a7446149b083dc0b
shivrastogi <shivrastogi01@gmail.com>
2021-11-18 08:54:53 +0530

    height checker

---
+++ HeightChecker.java
@@ -0,0 +1,23 @@
+class HeightChecker {
+    public int heightChecker(int[] heights) {
+        int[] bucket = new int[101];
+        
+        for (int i : heights)
+            bucket[i]++;
+        
+        int ans = 0, currHeight = 0;
+        
+        for (int i = 0; i < heights.length; ++i) {
+            while (bucket[currHeight] == 0)
+                ++currHeight;
+            
+            if (currHeight != heights[i]) {
+                ++ans;
+            }
+            
+            bucket[currHeight]--;
+        }
+        
+        return ans;
+    }
+}
