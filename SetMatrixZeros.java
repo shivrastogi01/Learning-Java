5940569d14a661d51e0656ed02c135054a97bcc3
shivrastogi <shivrastogi01@gmail.com>
2021-12-05 23:45:33 +0530

    set matrix zeros

--- SetMatrixZeros.java
+++ SetMatrixZeros.java
@@ -10,6 +10,29 @@ class SetMatrixZeros{
 				arr[i][j]=sc.nextInt();
 			}
 		}
-	}
+		int [] row =new int[3];
+		int [] col = new int [3];
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				if(arr[i][j]==0){
+				row[i]=-1;
+				col[i]=-1;
+				}
+			}
+		}
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				if(row[i]==-1 || col[j]==-1){
+					arr[i][j]=0;
+				}
+			}
+		}
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				System.out.print(arr[i][j]  +" ");
+			}
+			System.out.println("");
+		}
+		}
 
 	}
