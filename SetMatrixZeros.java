6241b1ea862ac635b748eb4a5e0e90763d708be7
shivrastogi <shivrastogi01@gmail.com>
2021-12-05 23:23:57 +0530

    set matrix zeros

---
+++ SetMatrixZeros.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+class SetMatrixZeros{
+	public static void main(String [] args){
+		int arr[][]= new int[3][3];
+		Scanner sc= new Scanner(System.in);
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				arr[i][j]=sc.nextInt();
+			}
+		}
+	}
+
+	}
