a2c5dc12fc25f9b58bcc3c9c01e4af2137758378
shivrastogi <shivrastogi01@gmail.com>
2021-12-23 08:55:50 +0530

    assign cookies

---
+++ AssignCookies.java
@@ -0,0 +1,20 @@
+
+class AssignCookies{
+public int findContentChildren(int[] g, int[] s) {
+     	Arrays.sort(g);
+     	Arrays.sort(s);
+     	int m = g.length,n = s.length;
+     	int i=0,j=0;
+     	int satisfied = 0;
+
+     	while (i < m && j < n) {
+     		if(g[i] <= s[j]){
+     			satisfied++;
+     			i++;j++;
+     		}else{
+     			j++;
+     		}
+     	}
+     	return satisfied;
+    }
+}
