a921ebb0fcd39b3e82cdc6ecb9b06b09310a6d53
shivrastogi <shivrastogi01@gmail.com>
2022-02-11 08:46:37 +0530

    FinfElementsInRotatedArray.java

---
+++ FinfElementsInRotatedArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class FinfElementsInRotatedArray {
+    static int search(int arr[], int target) {
+        int low = 0, high = arr.length - 1; //<---step 1
+
+        while (low <= high) { //<--- step 2
+            int mid = (low + high) >> 1; //<----step 3
+            if (arr[mid] == target)
+                return mid; // <---step 4
+
+            if (arr[low] <= arr[mid]) { //<---step 5
+                if (arr[low] <= target && arr[mid] >= target)
+                    high = mid - 1; //<---step 6 
+                else
+                    low = mid + 1; //<---step 7
+            } else { //<---step 7
+                if (arr[mid] <= target && target <= arr[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1; //<---step 8
+    }
+    public static void main(String args[]) {
+        int arr[] = {4,5,6,7,0,1,2,3};
+        int target = 3;
+        System.out.println("The index in which the number is present is " + search(arr, target));
+    }
+}
