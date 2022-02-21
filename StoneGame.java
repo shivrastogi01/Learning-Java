b57a8181e224c731c51df780b1da4b0590418bee
shivrastogi <shivrastogi01@gmail.com>
2022-02-21 10:18:47 +0530

    StoneGame.java

--- StoneGame.java
+++ StoneGame.java
@@ -1,42 +1,15 @@
-class StoneGame{ 
-
- Map[] startToMToRes = {new HashMap<Integer, Map<Integer, int[]>>(), 
-                           new HashMap<Integer, Map<Integer, int[]>>()};
-    
-    public int stoneGameII(int[] piles) {
-        return helper(piles, 0, 1, true)[0];
-    }
-    
-    private int[] helper(int[] piles, int start, int M, boolean turnA) {
-        if(start >= piles.length) {
-            return new int[] {0,0};
+class StoneGame {
+    public boolean stoneGame(int[] piles) {
+        int start=0;
+        int end = piles.length-1;
+        int alice =0;
+        int bob = 0;
+        while(start<end){
+            alice+=Math.max(piles[start],piles[end]);
+            bob+=Math.min(piles[start],piles[end]);
+            start++;
+            end--;
         }
-        
-        Map<Integer, int[]> mToRes = (Map) startToMToRes[turnA ? 0 : 1]
-            .getOrDefault(start, new HashMap<Integer, int[]>());
-        if(mToRes.containsKey(M)) {
-            return mToRes.get(M);
-        }
-        
-        int sum = 0;
-        int[] res = new int[] {0, 0};
-        for(int i = 0; i < 2 * M && start + i < piles.length; i++) {
-            sum += piles[start + i];
-
-            int mNew = Math.max(M, i + 1);
-            int[] childRes = helper(piles, start + i + 1, mNew, !turnA);
-            
-            int a = childRes[0] + (turnA ? sum : 0);
-            int b = childRes[1] + (!turnA ? sum : 0);
-            
-            if((turnA && a > res[0]) || (!turnA && b > res[1])) {
-                res[0] = a;   
-                res[1] = b;   
-            }
-        }
-        
-        mToRes.put(M, res);
-        startToMToRes[turnA ? 0 : 1].put(start, mToRes);
-        return res;
+        return alice>bob;
     }
 }
