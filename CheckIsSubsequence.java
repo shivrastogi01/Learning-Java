f6c4be6a9147e035c7d8709da4c3a5925a044942
shivrastogi <shivrastogi01@gmail.com>
2022-03-02 10:34:03 +0530

    check is subsequence

---
+++ CheckIsSubsequence.java
@@ -0,0 +1,19 @@
+class CheckIsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        
+        int lens=s.length();
+        int lent=t.length();
+        int i=0;
+        int j=0;
+        while(i<lent){
+           // int j=0;
+            
+           if(j<lens&&s.charAt(j)==t.charAt(i)){
+           j++;    
+           }
+            i++;
+            
+        }
+        return (j==lens)?true:false;
+    }
+}
