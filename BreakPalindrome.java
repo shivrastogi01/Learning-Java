977337c9c82b6ed73b54dd680096132a738fdceb
shivrastogi <shivrastogi01@gmail.com>
2022-03-08 00:13:46 +0530

    BreakPalindrome.java

---
+++ BreakPalindrome.java
@@ -0,0 +1,17 @@
+class BreakPalindrome {
+    public String breakPalindrome(String palindrome) {
+        if(palindrome.length() == 0 || palindrome.length() == 1){
+            return "";
+        }
+        char[] ch = palindrome.toCharArray();
+        for(int i = 0;i<ch.length/2;i++){
+            if(ch[i]-'a' != 0){
+                ch[i] = 'a';
+                return new String(ch);
+            }
+            
+        }
+        ch[ch.length-1] = 'b';
+        return new String(ch);
+    }
+}
