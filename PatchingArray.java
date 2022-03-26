142da4e2e3c64f94bdf0413043a120be8bb12182
shivrastogi <shivrastogi01@gmail.com>
2022-03-26 22:19:33 +0530

    PatchingArray

---
+++ PatchingArray.java
@@ -0,0 +1,20 @@
+class PatchingArray {
+    public int minPatches(int[] nums, int n) {
+        int patches = 0, i = 0;
+        long val = 1;
+        
+        while(val <= n){
+            if(i < nums.length && nums[i] <= val){
+                val += nums[i++];
+            }
+            else{
+                val += val;
+                patches++;
+            }
+            
+            //System.out.println(val);
+        }
+        
+        return patches;
+    }
+}
