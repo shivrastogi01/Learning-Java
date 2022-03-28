d26277835627087c70d4407145a8cc10b5e35486
shivrastogi <shivrastogi01@gmail.com>
2022-03-28 08:05:42 +0530

    SearchInRotatedSortedArray

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,9 @@
+class SearchInRotatedSortedArray {
+    public boolean search(int[] nums, int target) {
+        for(int i=0 ; i<nums.length ; i++)
+        {
+            if(nums[i] == target) return true;
+        }
+        return false;
+    }
+}
