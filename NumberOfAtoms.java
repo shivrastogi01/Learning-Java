b750b2d108ec73b63ca83744d917ab454963f0aa
shivrastogi <shivrastogi01@gmail.com>
2022-04-22 09:57:21 +0530

    NumberOfAtoms

---
+++ NumberOfAtoms.java
@@ -0,0 +1,69 @@
+class NumberOfAtoms {
+    int i, n;
+    String formula;
+
+    public String countOfAtoms(String formula) {
+        this.i = 0;
+        this.n = formula.length();
+        this.formula = formula;
+
+        Deque<Map<String, Integer>> stack = new LinkedList<Map<String, Integer>>();
+        stack.push(new HashMap<String, Integer>());
+        while (i < n) {
+            char ch = formula.charAt(i);
+            if (ch == '(') {
+                i++;
+                stack.push(new HashMap<String, Integer>()); // Push an empty hash table onto the stack, ready to count the number of atoms in parentheses
+            } else if (ch == ')') {
+                i++;
+                int num = parseNum(); // numbers to the right of parentheses
+                Map<String, Integer> popMap = stack.pop(); // Pop the number of atoms in parentheses
+                Map<String, Integer> topMap = stack.peek();
+                for (Map.Entry<String, Integer> entry : popMap.entrySet()) {
+                    String atom = entry.getKey();
+                    int v = entry.getValue();
+                    topMap.put(atom, topMap.getOrDefault(atom, 0) + v * num); // Multiply the number of atoms in parentheses by num and add to the number of atoms in the previous layer
+                }
+            } else {
+                String atom = parseAtom();
+                int num = parseNum();
+                Map<String, Integer> topMap = stack.peek();
+                topMap.put(atom, topMap.getOrDefault(atom, 0) + num); // Count the number of atoms
+            }
+        }
+
+        Map<String, Integer> map = stack.pop();
+        TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>(map);
+
+        StringBuffer sb = new StringBuffer();
+        for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
+            String atom = entry.getKey();
+            int count = entry.getValue();
+            sb.append(atom);
+            if (count > 1) {
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
+
+    public String parseAtom() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(formula.charAt(i++)); // scan initials
+        while (i < n && Character.isLowerCase(formula.charAt(i))) {
+            sb.append(formula.charAt(i++)); // Scan lowercase letters after the first letter
+        }
+        return sb.toString();
+    }
+
+    public int parseNum() {
+        if (i == n || !Character.isDigit(formula.charAt(i))) {
+            return 1; // its not number, 
+        }
+        int num = 0;
+        while (i < n && Character.isDigit(formula.charAt(i))) {
+            num = num * 10 + formula.charAt(i++) - '0'; // scannen number
+        }
+        return num;
+    }
+}
