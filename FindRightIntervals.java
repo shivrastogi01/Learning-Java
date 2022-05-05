291ea141a696c3b36e10253c5196f6cf763c056d
shivrastogi <shivrastogi01@gmail.com>
2022-05-05 08:59:06 +0530

    FindRightIntervals.java

---
+++ FindRightIntervals.java
@@ -0,0 +1,38 @@
+class FindRightIntervals {
+    public int[] findRightInterval(int[][] intervals) {
+       int res[] = new int[intervals.length];
+        HashMap<String,Integer> hm = new HashMap<>();
+        for(int i=0;i<intervals.length;i++){
+            String key = intervals[i][0]+":"+intervals[i][1];
+            hm.put(key, i);
+        }
+        Arrays.sort(intervals, new SortByFirst());
+        for(int interval[]:intervals){
+            String key = interval[0]+":"+interval[1];
+            int pos =binarySearch(hm,intervals,interval[1]);       
+                res[hm.get(key)]= pos;
+        } 
+        return res;
+    }
+    public int binarySearch(HashMap<String,Integer> hm, int[][] intervals, int end1){
+        int low=0,end=intervals.length-1,res=-1;
+        while(low<=end){
+            int mid=low+(end-low)/2;
+            if(intervals[mid][0]==end1)
+                return hm.get(intervals[mid][0]+":"+intervals[mid][1]);
+            else if(intervals[mid][0]>end1){
+                res=hm.get(intervals[mid][0]+":"+intervals[mid][1]);
+                end=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return res;
+    }
+}
+class SortByFirst implements Comparator<int[]>{
+    public int compare(int[] val1, int[] val2){
+        return val1[0]-val2[0];
+    }
+}
