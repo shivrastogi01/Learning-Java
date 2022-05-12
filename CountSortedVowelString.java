66277fd0b29a5926a4fafc512d096cacefdca4e5
shivrastogi <shivrastogi01@gmail.com>
2022-05-12 06:46:26 +0530

    CountSortedVowelString

---
+++ CountSortedVowelString.java
@@ -0,0 +1,16 @@
+class CountSortedVowelString {
+    public int countVowelStrings(int n) {
+        int a = 1;
+        int e = 1;
+        int i = 1;
+        int o = 1;
+        int u = 1;
+        for (int j = 1; j < n; j++) {
+            o = o + u;
+            i = i + o;
+            e = e + i;
+            a = a + e;
+        }
+        return a + e + i + o + u;
+    }
+}
