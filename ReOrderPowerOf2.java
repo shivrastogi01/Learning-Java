5a818f77aa91e008d511eccb8448cf042c8d4438
shivrastogi <shivrastogi01@gmail.com>
2022-05-29 00:07:19 +0530

    'ReOrderPowerOf2

---
+++ ReOrderPowerOf2.java
@@ -0,0 +1,21 @@
+class ReOrderPowerOf2 {
+    public boolean reorderedPowerOf2(int n) {
+        Set<Long> two = new HashSet<>();
+        for (int i = 1; i <= (int)1e9; i <<= 1){
+            two.add(transform(i));
+        }
+
+        return two.contains(transform(n));
+    }
+
+    private long transform(int n){
+        long sum = 0;
+        while(n > 0){
+            int d = n % 10;
+            sum += 1L << (d*3);
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
