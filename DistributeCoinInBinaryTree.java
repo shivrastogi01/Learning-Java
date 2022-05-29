86cac361c51408cfeaa03bbcc529e5f8adc6c23c
shivrastogi <shivrastogi01@gmail.com>
2022-05-29 16:31:58 +0530

    DistributeCoinInBinaryTree

---
+++ DistributeCoinInBinaryTree.java
@@ -0,0 +1,17 @@
+class DistributeCoinInBinaryTree {
+int ans =0;
+public int distributeCoins(TreeNode root) {
+dfs(root);
+return ans;
+}
+private int dfs(TreeNode root){
+if(root ==null){
+return 0;
+}
+int left = dfs(root.left);
+int right = dfs(root.right);
+ans+= Math.abs(left) +Math.abs(right);
+
+    return root.val+left+right-1;
+}
+}
