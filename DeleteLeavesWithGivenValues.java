fb8ae0d15619998a2e81819a71b13fb652040a39
shivrastogi <shivrastogi01@gmail.com>
2022-09-18 10:35:49 +0530

    DeleteLeavesWithGivenValues

---
+++ DeleteLeavesWithGivenValues.java
@@ -0,0 +1,19 @@
+class DeleteLeavesWithGivenValues {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root == null)
+            return null;
+        
+        if(root.val == target && root.left == null && root.right == null){
+            return null;
+        }
+        
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        if(root.val == target && root.left == null && root.right == null){
+            return null;
+        }
+
+        return root;
+    }
+}
