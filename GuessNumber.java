a74707cd42017b279070250f865ed32b2df4d1fe
shivrastogi <shivrastogi01@gmail.com>
2022-10-04 10:40:32 +0530

    GuessNumber

---
+++ GuessNumber.java
@@ -0,0 +1,23 @@
+class GuessNumber{
+   public int getMoneyAmount(int n) {
+
+	int dpRows = n+2;
+	int dpCols = n+2;
+	int[][] dp = new int[dpRows][dpCols];
+
+	for(int lenSubArray = 1; lenSubArray<n; lenSubArray++) {
+		for(int start = 1; start+lenSubArray<=n; start++) {
+			int end = start + lenSubArray;
+
+			dp[start][end] = Integer.MAX_VALUE;
+			for(int guess = start; guess<=end; guess++) {
+
+				// taking worst case
+				int costOfGuess = guess + Math.max(dp[start][guess-1], dp[guess+1][end]);
+				dp[start][end] = Math.min(dp[start][end], costOfGuess);
+			}
+		}  
+	}
+	return dp[1][n];
+}
+}
