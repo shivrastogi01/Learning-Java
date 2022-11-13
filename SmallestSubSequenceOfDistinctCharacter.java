25433a9bdc53750f8f6f75db7efedb7c31405de7
shivrastogi <shivrastogi01@gmail.com>
2022-11-13 08:33:43 +0530

    SmallestSubSequenceOfDistinctCharacter

---
+++ SmallestSubSequenceOfDistinctCharacter.java
@@ -0,0 +1,31 @@
+class SmallestSubSequenceOfDistinctCharacter{
+    public String smallestSubsequence(String s) {
+        int [] lastIndex=new int[26];
+        for(int i=0;i<s.length();i++)
+            lastIndex[s.charAt(i)-'a'] = i;
+            boolean [] seen=new boolean[26];
+            Stack<Integer> st=new Stack<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            int c=s.charAt(i) - 'a';
+            
+            if(seen[c]) continue;
+            
+            while(!st.isEmpty() && st.peek()>c && i < lastIndex[st.peek()])
+            {
+                seen[st.pop()] = false;
+            }
+            st.push(c);
+            seen[c]=true;
+        }
+        StringBuilder sb=new StringBuilder();
+        
+        while(!st.isEmpty())
+        {
+            sb.append((char) (st.pop() + 'a'));
+        }
+        return sb.reverse().toString();
+        
+    }
+}
